0d23ddd363d6787b50543652e70ba4682b3d51d6
Noèlia <noeliagoca28@gmail.com>
2026-02-11 23:30:49 +0100

    Delete Portafolio-Web directory

--- Portafolio-Web/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarSeccio(id) {
-    let seccions = ["sobre", "projectes", "contacte"];
-    for (let i = 0; i < seccions.length; i++) {
-        document.getElementById(seccions[i]).style.display = "none";
-    }
-    document.getElementById(id).style.display = "block";
-}
